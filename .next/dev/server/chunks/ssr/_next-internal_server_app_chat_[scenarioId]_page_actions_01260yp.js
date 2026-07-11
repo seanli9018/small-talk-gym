4e00419984b25b694f7dd359c604b9c4734e2bbc
seanli9018 <seanli9018@gmail.com>
2026-07-11 11:04:34 -0500

    init3

--- .next/dev/server/chunks/ssr/_next-internal_server_app_chat_[scenarioId]_page_actions_01260yp.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/chat/[scenarioId]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_chat_%5BscenarioId%5D_page_actions_01260yp.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yeE1H1xqJr6BSIUX8zml8ikgJzgWOYyDNBf6tDbTVAw=\"\n}"
